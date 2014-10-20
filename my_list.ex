97bdb97ddc5ea99ec434649c26c96e39447d21fa
no-glue <nikola_pav@eunet.rs>
2014-10-20 23:39:48 +0200

    reduce

--- my_list.ex
+++ my_list.ex
@@ -4,4 +4,10 @@ defmodule MyList do
   # _head does nothing, just placeholder here (_)
   def map([], _func), do: []
   def map([item | items], func), do: [func.(item) | map(items, func)]
+  # map values to something
+  def reduce([], _func, total), do: total
+  def reduce([item | items], func, total), do: reduce(items, func, func.(item, total))
+  def reduce([item | items], func), do: reduce(items, func, func.(item, 0))
+  # reduce
+  def sum(items), do: reduce(items, fn(item, total)->item + total end)
 end
