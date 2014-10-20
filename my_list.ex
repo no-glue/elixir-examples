b4081c4b148760ececf5c69200c7aeea829bba3c
no-glue <nikola_pav@eunet.rs>
2014-10-20 23:24:36 +0200

    map

--- my_list.ex
+++ my_list.ex
@@ -1,4 +1,7 @@
 defmodule MyList do
   def len([]), do: 0
   def len([_head | tail]), do: 1 + len(tail)
+  # _head does nothing, just placeholder here (_)
+  def map([], _func), do: []
+  def map([item | items], func), do: [func.(item) | map(items, func)]
 end
